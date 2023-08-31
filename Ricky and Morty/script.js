a2d0d7643f7456e306323ce9cf8d110081c6d1cb
lorena resquin <119911211+lorenaresquin@users.noreply.github.com>
2023-08-31 00:13:09 -0300

    https://github.com/lorenar31/tp.git

--- Ricky and Morty/script.js
+++ Ricky and Morty/script.js
@@ -1,47 +1,35 @@
-//campo de busqueda con el ID
-// Hacemos una solicitud a la API utilizando el ID proporcionado
+//campo de busqueda con el ID// Hacemos una solicitud a la API utilizando el ID proporcionado
 
-//const traerDatos = async (id) => {
-//const buscarDatos = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
-//.then(response => response.json()) 
-//.then(data => console.log(data));
-//}
-//traerDatos();
-
+const traerDatos = async (id) => {
+const buscarDatos = await fetch(`https://rickandmortyapi.com/api/character/`)
+.then(response => response.json()) 
+.then(data => console.log(data));
+}
+traerDatos()
 
 //crear tarjeta con template strings
 
-//const crearCard = ()=> {
-  //  return `
-    //    <div class="card" style="width: 18rem">
-    //    <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"${data.image} alt="${data.name}">
-      //  <div class="card-body">
-     //   <h5>${data.name}</h5>
-     //   <p>${data.species}</p>
-      //  <p>${data.status}</p>
-      //  </div>
-      //  </div>`;
-//}
+        const crearCard = () => {
+        return `
+        <div class="card-container" style="width: 18rem">
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"" alt="card-img">
+                <div class="card-body">
+                <h4 class="card">${userData.name}</h4>
+                <p class="card">${userData.species}</p>
+                <p class="card">${userData.status}</p>
+            </div>
+          </div>`;
+}
 
-//const cardData = () => {
-//const contenedor = document.querySelector('.card-container)
-
-//const newCard = crearCard()
-//contenedor.innerHTML += newCard
-//}
-
-//Agregar función al boton y al input
-
-//const agragarClick = () => {
- //   const input = document.querySelector("input#personaje-id");
- //   const button = document.querySelector("button#btn-button");
-    
-   // input.addEventListener('input', function(event) {
-     //   console.log('click',(e).value);
-    //});
-    
-   // button.addEventListener('click', function() {
-      //  console.log('onclick',(e));
-     //   alert('hice click')
-    //});
-   // }
+const cardData = () => {
+const contenedor = document.querySelector('.card-container')
+const newCard = crearCard()
+contenedor.innerHTML += newCard
+}
+//Agregar función al boton
+    const hacerClick = () => {
+    const button = document.querySelector("#btn-button");
+    button.addEventListener('click', (e)=>{
+     cardData()
+    })
+}
