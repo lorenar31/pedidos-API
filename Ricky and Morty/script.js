f7399735e932bedc1072c12dd96214a07c802122
lorena resquin <119911211+lorenaresquin@users.noreply.github.com>
2023-08-30 22:47:16 -0300

    https://github.com/lorenar31/tp.git

---
+++ Ricky and Morty/script.js
@@ -0,0 +1,47 @@
+//campo de busqueda con el ID
+// Hacemos una solicitud a la API utilizando el ID proporcionado
+
+//const traerDatos = async (id) => {
+//const buscarDatos = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+//.then(response => response.json()) 
+//.then(data => console.log(data));
+//}
+//traerDatos();
+
+
+//crear tarjeta con template strings
+
+//const crearCard = ()=> {
+  //  return `
+    //    <div class="card" style="width: 18rem">
+    //    <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"${data.image} alt="${data.name}">
+      //  <div class="card-body">
+     //   <h5>${data.name}</h5>
+     //   <p>${data.species}</p>
+      //  <p>${data.status}</p>
+      //  </div>
+      //  </div>`;
+//}
+
+//const cardData = () => {
+//const contenedor = document.querySelector('.card-container)
+
+//const newCard = crearCard()
+//contenedor.innerHTML += newCard
+//}
+
+//Agregar función al boton y al input
+
+//const agragarClick = () => {
+ //   const input = document.querySelector("input#personaje-id");
+ //   const button = document.querySelector("button#btn-button");
+    
+   // input.addEventListener('input', function(event) {
+     //   console.log('click',(e).value);
+    //});
+    
+   // button.addEventListener('click', function() {
+      //  console.log('onclick',(e));
+     //   alert('hice click')
+    //});
+   // }
